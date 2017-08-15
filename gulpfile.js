4794dc4035737f89cbd80a29c656a009968de0da
Artem Kikhtenko <artemka81@gmail.com>
2017-08-15 19:08:32 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+        	port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+// PUG
+gulp.task('templates:compile', function buildHTML() {
+  return gulp.src('source/template/index.pug')
+  .pipe(pug({
+    pretty:true
+  }))
+  .pipe(gulp.dest('build'))
+});
+
+//SASS
+gulp.task('styles:compile', function () {
+  return gulp.src('source/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'));
+});
+
+// WATCH
+gulp.task('watch', function(){
+	gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
+	gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+});
+
+// DEFAULT
+gulp.task('default', gulp.series(
+	gulp.parallel('templates:compile','styles:compile'),
+	gulp.parallel('watch', 'server')
+));
+
+
+
+
+
